2e3bc60be7d079a570bfb0d7cf0d937b6dc5d919
Kuba <jkalamaszek@gmail.com>
2024-02-09 10:13:29 +0100

    Add menu bar

---
+++ Home.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.JPG", width=500)
+
+with col2:
+    st.title('Jakub Kalamaszek')
+    content = """
+    Hi, I am Jakub Kalamaszek,  I am 2 degree student of Automation control an Metrology. I'd like to be programmer
+    """
+    st.info(content)
+
+
+content2 = """
+Below you can find some of teh apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
